e8bc48beabc852e2bf40d96eb3d8b477f5bad406
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2018-05-01 11:43:23 +0200

    Add read from standard input

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace read_file_find_closest_element
+namespace RecruitmentTask
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!Console.IsInputRedirected)
+            {
+                Console.WriteLine("This program requires that input is redirected from a file.");
+                return;
+            }
+
+            var inputStream = Console.In;
+
+            try
+            {
+                String line = inputStream.ReadToEnd();
+                Console.WriteLine(line);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
